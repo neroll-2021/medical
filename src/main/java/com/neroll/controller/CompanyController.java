d71142071be124718c047e9e72f1ea0ea019a0e2
straw <2780182584@qq.com>
2024-12-29 11:31:03 +0800

    为部分 Controller 类添加 @CrossOrigin 注解，解决跨域问题

--- src/main/java/com/neroll/controller/CompanyController.java
+++ src/main/java/com/neroll/controller/CompanyController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/company")
 public class CompanyController {
--- src/main/java/com/neroll/controller/UserController.java
+++ src/main/java/com/neroll/controller/UserController.java
@@ -4,14 +4,12 @@ import com.neroll.pojo.Result;
 import com.neroll.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
     @Autowired
     private UserService userService;
-
     @PostMapping("/login")
     public Result<String> login(String username, String password) {
         System.out.println(username + ", " + password);
